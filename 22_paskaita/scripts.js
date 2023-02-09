58ca24ff543edecddadedab0727ce469fce3e539
ritullia <brigrita@gmail.com>
2023-02-09 23:15:50 +0200

    commit

--- 22_paskaita/scripts.js
+++ 22_paskaita/scripts.js
@@ -2,22 +2,10 @@ console.log('veikia')
 
 //https://stackoverflow.com/questions/11508463/javascript-set-object-key-by-variable
 
-let url = 'https://httpdump.app/dumps/3444c85b-2476-4ca1-a7dd-3c142e992349'
+let url = 'https://httpdump.app/dumps/5a83f64a-5ea9-433b-8aba-ee360d1d1cea'
 
-// fetch('url')
-//   .then(response => {
-//     //handle response            
-//     console.log(response);
-//   })
-//   .then(data => {
-//     //handle data
-//     console.log(data);
-//   })
-//   .catch(error => {
-//     //handle error
-//   });
 
-// 1. Paspaudziam ant mygtuko paleidziam funkcija.
+// 1. Paspaudziam ant mygtuko paleidziam formos funkcija.
 
 async function formSubmit(event) {
 
@@ -30,7 +18,7 @@ async function formSubmit(event) {
     console.log(infoCards)
 
     //  naujas div kur imesim sukurta info
-    let newDiv = document.getElementById('main-container');
+    let newDiv = document.getElementById('card-wrapper');
     let seconDiv = document.createElement('div')
     seconDiv.innerHTML = '';
 
@@ -39,7 +27,7 @@ async function formSubmit(event) {
     let cardArr = [];
 
 
-    // infocrads inputu for loop
+    // infocrads inputu for loop (gauta sarasa isimetam i for loop arba i forEach) 
     for (let i = 0; i < infoCards.length; i++) {
         // console.log(infoCards[i].querySelectorAll('.info'))
 
@@ -52,31 +40,43 @@ async function formSubmit(event) {
             // console.log(input)
             const typeInp = input.getAttribute('name')
             const valInp = input.value
-
             // console.log(typeInp, valInp)
-
 
             cardObj[typeInp] = valInp;
         
         })
 
         cardArr.push(cardObj);
-
+        
 
         // 4. Issiunciam i serveri info objekto
-        // fetch(url, {
-        //     method: 'POST',
-        //     body: JSON.stringify(cardArr),
-        // }).then(function (response) {
-        //     return response.json()
-        // }).catch(error => console.error('Error:', error));
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(cardArr),
+        }).then(function (response) {
+            return response.json()
+        }).catch(error => console.error('Error:', error));
     }
-    console.log(cardArr)
 
-    newDiv.append(JSON.stringify(cardArr))
+    let name = document.createElement('h3')
+    name.textContent = 'Name:'
+    let city = document.createElement('h3')
+    city.textContent = 'City:'
+    let brand = document.createElement('h3')
+    brand.textContent = 'Brand:'
+    let model = document.createElement('h3')
+    model.textContent = 'Model:'
+
+    seconDiv.append(name, city, brand, model)
+    console.log(seconDiv)
+    
+
+    console.log(cardArr)
+    newDiv.append(seconDiv)
+    
 }
 
 
-
+// formos paleidimas, turi buti submit formoai paleisti
 let btn = document.querySelector('#my-form');
 btn.addEventListener('submit', formSubmit)
